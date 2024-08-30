a4e8785d29f89c7dadf290a452fcf10001ddb03f
memoneri15 <memoneri15@hotmail.com>
2024-08-30 08:44:21 -0600

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/memoneri15.py
@@ -0,0 +1,29 @@
+# http//www.python.org
+
+# Comentario en una linea
+
+"""
+Esto tambien es un
+comentario en
+varias lineas
+"""
+
+'''
+Esto tambien es 
+un comentario en 
+varias lineas
+'''
+
+my_variable = "Mi variable"
+my_variable = 'Nuevo valor de mi variable'
+
+MY_CONSTANT = "Mi constante"
+
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_string = "Mi cadena de texto"
+my_other_string = 'Mi otra cadena de texto'
+
+print("!Hola, Python¡")
