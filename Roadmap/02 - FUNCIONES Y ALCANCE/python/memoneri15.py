6b4cc947c30f551d1e509b21af21c95577782e2b
memoneri15 <memoneri15@hotmail.com>
2024-09-06 08:06:41 -0600

    #02 - Python

--- Roadmap/02 - FUNCIONES Y ALCANCE/python/memoneri15.py
+++ Roadmap/02 - FUNCIONES Y ALCANCE/python/memoneri15.py
@@ -119,13 +119,37 @@ print("MoureDev".upper())
 Variables locales y globales
 """
 
-global_variable = "Python"
-
-print(global_variable)
+global_var = "Python"
 
 
 def hello_python()
-  print(f"Hello, {global_variable}")
+  local_var = "Hola"
+  print(f"{local_var}, {global_var}!")
 
+
+print(global_var)
+# print(local_var) No se puede acceder desde fuera de la funcion
 
 hello_python()
+
+
+"""
+Extra
+"""
+
+def print_numbers(text_1, text_2)-> int:
+count = 0
+  for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+      print(text_1 + text_2)
+    elif number % 3 == 0:
+      print(text_1)
+    elif number % 5 == 0:
+      print(text_2)
+    else:
+      print(number)
+      count += 1
+  return count
+    
+
+print(print_numbers("Fizz", "Buzz"))
