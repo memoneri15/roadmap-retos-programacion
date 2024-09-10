5869d8ccdcf2f965196650d79f79ae3aaac35c09
memoneri15 <memoneri15@hotmail.com>
2024-09-10 08:36:25 -0600

    #03 - Python

--- Roadmap/03 - ESTRUCTURAS DE DATOS/python/memoneri15.py
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/python/memoneri15.py
@@ -1,3 +1,7 @@
+"""
+Extra
+"""
+
 # Listas
 my_list = ["Brais", "Bl4ck", "Wolfy", "Visionos"]
 print(my_list)
@@ -34,4 +38,65 @@ print(my_set)
 print(type(my_set))
 
 # Diccionario
+my_dict: dict = {
+  "name":"Brais",
+  "surname":"Moure",
+  "alias":"@mouredev",
+  "age":"36"
+}
+my_dict.["email"] = "memoneri15@hotmail.com" # Inserccion
+print(my_dict)
+del my_dict["surname"] # Eliminacion
+print(my_dict)
+print(my_dict["name"]) # Acceso
+my_dict["age"] = "37" # Actualizacion
+print(my_dict)
+my_dict = dict(sorted(my_dict.items())) # Ordenación
+print(my_dict)
+print(type(my_dict))
 
+"""
+Extra
+"""
+
+
+def my_agenda():
+
+  agenda = {}
+
+  while True:
+  
+  print("")
+  print("1. Buscar contacto")
+  print("2. Insertar contacto")
+  print("3. Actualizar contacto")
+  print("4. Eliminiar contacto")
+  print("5. Salir")
+
+
+  option = input("\nSelecciona una opción: ")
+
+  match option:
+      case "1":
+          pass
+      case "2":
+          name = input("Introduce el nombre del contacto: ")
+          phone = input("Introduce el teléfono del contacto: ")
+          if phone.isdigit() and len(phone)  > 0 and len(phone) <= 11: 
+              agenda[name] = phone
+          else:
+              print(
+                  "Debes introducir un número de teléfono con un máximo de 11 dígitos.")
+          pass
+      case "3":
+          pass
+      case "4":
+          pass
+      case "5":
+          print("Saliendo de la agenda.")
+          break
+      case _:
+          print("Opción no válida. Elige una opción del 1 al 5.")
+
+
+my_agenda()
