7c58900641afee0dedecd6bd5707932ff3948998
memoneri15 <memoneri15@hotmail.com>
2024-09-11 08:20:58 -0600

    #03 - Python

--- Roadmap/03 - ESTRUCTURAS DE DATOS/python/memoneri15.py
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/python/memoneri15.py
@@ -64,6 +64,14 @@ def my_agenda():
 
   agenda = {}
 
+  def insert_contact():
+       phone = input("Introduce el teléfono del contacto: ")
+          if phone.isdigit() and len(phone)  > 0 and len(phone) <= 11: 
+              agenda[name] = phone
+          else:
+              print(
+                  "Debes introducir un número de teléfono con un máximo de 11 dígitos.")
+            
   while True:
   
   print("")
@@ -73,26 +81,32 @@ def my_agenda():
   print("4. Eliminiar contacto")
   print("5. Salir")
 
-
   option = input("\nSelecciona una opción: ")
 
   match option:
       case "1":
-          pass
+          name = input("Introduce el nombre del contacto a buscar: ")
+          if name in agenda:
+              print(
+                  f"el número de télefono de {name} es {agenda[name]}.")
+          else:
+              print(f"El contacto {name} no existe.")
       case "2":
           name = input("Introduce el nombre del contacto: ")
-          phone = input("Introduce el teléfono del contacto: ")
-          if phone.isdigit() and len(phone)  > 0 and len(phone) <= 11: 
-              agenda[name] = phone
-          else:
-              print(
-                  "Debes introducir un número de teléfono con un máximo de 11 dígitos.")
-          pass
+          insert_contact() 
       case "3":
-          pass
+          name = input("Introduce el nombre del contacto a actualizar: ")
+          if name in agenda
+             insert_contact()
+          else:
+              print(f"El contacto {name} no existe. ")        
       case "4":
-          pass
-      case "5":
+          name = input("Introduce el nombre del contacto a eliminar: ")
+          if name in agenda: 
+              del agenda[name]
+          else:
+              print(f"El contacto {name} no existe. ")
+         case "5":
           print("Saliendo de la agenda.")
           break
       case _:
