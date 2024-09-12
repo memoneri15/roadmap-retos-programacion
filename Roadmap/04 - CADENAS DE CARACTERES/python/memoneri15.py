7211f6ca75e0db29e8e308d88ffd986f5f9b45cc
memoneri15 <memoneri15@hotmail.com>
2024-09-12 08:33:35 -0600

    #04 - Python

--- Roadmap/04 - CADENAS DE CARACTERES/python/memoneri15.py
+++ Roadmap/04 - CADENAS DE CARACTERES/python/memoneri15.py
@@ -1,1 +1,59 @@
+"""
+Operaciones
+"""
+s1 = "Hola"
+s2 = "Pythonthon"
 
+# Concatenacion
+print(s1 + ", " + s2 + "!")
+
+# Repetición
+print(s1 * 3)
+
+# Indexación
+print(s1[0] + s1[1] + s1[2] + s1[3])
+
+# Longitud
+print(len(s2))
+
+# Slicing (porción)
+print(s2[2:6])
+print(s2[2:])
+print(s2[0:2])
+print([:2])
+
+# Busqueda
+print("a" in s1)
+print("i" in s1)
+
+# Reemplazo
+print(s1.replace("o", "a"))
+
+# División
+print(s2.split("t"))
+
+# Mayúsculas, minúsculas y letras en mayúscula
+print(s1.upper())
+print(s1.lower())
+print("brais moure".title())
+print("brais moure".capitalize())
+
+# Eliminación de espacios al principio y al final
+print(" brais moure ".strip())
+
+# Búsqueda al principio y al final
+print(s1.startwith("Ho"))
+print(s1.startwith("Py"))
+print(s1.endswith("la"))
+print(s1.endswith("thon"))
+
+s3 = "Brais Moure @mouredev"
+
+# Búsqueda de posición
+print(s3.find("moure"))
+print(s3.find("Moure"))
+print(s3.find(M))
+print(s3.lower().find("m"))
+
+# Búsqueda de ocurrencias
+print(s3.lower().count("m"))
