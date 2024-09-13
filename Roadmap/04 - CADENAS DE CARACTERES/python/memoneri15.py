b3342c38a15779dd318f2c5bf729e527f3e3740a
memoneri15 <memoneri15@hotmail.com>
2024-09-13 08:29:26 -0600

    #04 - Python

--- Roadmap/04 - CADENAS DE CARACTERES/python/memoneri15.py
+++ Roadmap/04 - CADENAS DE CARACTERES/python/memoneri15.py
@@ -57,3 +57,53 @@ print(s3.lower().find("m"))
 
 # Búsqueda de ocurrencias
 print(s3.lower().count("m"))
+
+# Formateo
+print("Saludo: {}, lenguaje: {}!".format(s1, s2))
+
+# Interpolación
+print(f"Saludo: {s1}, lenguaje: {s2}!")
+
+# Tranformación en lista de caracteres
+print(list(s3))
+
+# Transformación de lista en cadena
+l1 = [s1, ", ", s2, "!"]
+print("".join(l1))
+
+# Transformaciones númericas
+s4 = "123456"
+s4 = int(s4)
+print(s4)
+
+s5 = "123456.123"
+s5 = float(s5)
+print(s5)
+
+# Comprobaciones varias
+s4 = "123456"
+print(s1.isalnum())
+print(s1.isalpha())
+print(s4.isalpha())
+print(s4.isnumeric())
+
+
+"""
+Extra
+"""
+
+
+def check(word1: str, word2: str):
+
+    # Palíndromo
+    print(f"¿{word1} es un palíndromo?: {word1 == word1[::-1]}")
+    print(f"¿{word2} es un palíndromo?: {word2 == word2[::-1]}")
+
+    # Anagrama
+    print(f"¿{word1} es anagrama de {word2}?: {sorted(word1) == sorted.(word2)}")
+
+    # Isograma
+     
+
+check("radar", "python")
+check("amor", "roma")
