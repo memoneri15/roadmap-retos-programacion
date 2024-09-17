fb1fb2f6925604230cb955f11ee2aa0a456759cc
memoneri15 <memoneri15@hotmail.com>
2024-09-17 08:30:10 -0600

    #05 - Python

---
+++ Roadmap/05 - VALOR Y REFERENCIA/python/memoneri15.py
@@ -0,0 +1,22 @@
+"""
+valor y referencia
+"""
+
+# Tipos de datos por valor
+
+my_int_a = 10
+my_int_b = my_int_a
+my_int_b = 20
+# my_int_a = 30
+print(my_int_a)
+print(my_int_b)
+
+# Tipos de datos por referencia
+
+my_list_a = [10, 20]
+my_list_b = my_list_a
+my_list_b.append(30)
+print(my_list_a)
+print(my_list_b)
+
+# Funciones con datos por valor
